981f20494b81ae8215935c6f22bd4c0272ebfc6f
itaji-create <itaji.1999@gmail.com>
2022-12-11 12:13:27 -0300

    feat: adiciona função routes ao app da api

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
+const routes = require('../routes/index')
+
+app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+routes(app);
 
 module.exports = app;
