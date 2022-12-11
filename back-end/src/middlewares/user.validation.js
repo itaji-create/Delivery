06c20f06409b907b297254184f18b21191d75aec
itaji-create <itaji.1999@gmail.com>
2022-12-11 12:39:15 -0300

    feat: cria middleware para validar formato dos dados ao tentar cadastrar novo usuário

--- back-end/src/middlewares/user.validation.js
+++ back-end/src/middlewares/user.validation.js
@@ -6,6 +6,16 @@ const signInValidation = (req, res, next) => {
     next();
 };
 
+const signUpValidation = (req, res, next) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    const { name, email, password } = req.body;
+    if (name.length < 12) return res.status(400).json('invalid name');
+    if (!emailRegex.test(email)) return res.status(400).json('invalid email');
+    if (password.length < 6) return res.status(400).json('invalid password');
+    next();
+};
+
 module.exports = {
     signInValidation,
+    signUpValidation
 };
--- back-end/src/routes/user.routes.js
+++ back-end/src/routes/user.routes.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const { signIn, signUp } = require('../controllers/user.controller');
-const { signInValidation } = require('../middlewares/user.validation');
+const { signInValidation, signUpValidation } = require('../middlewares/user.validation');
 router
   .post('/signIn', signInValidation, signIn)
-  .post('/signUp', signUp);
+  .post('/signUp', signUpValidation, signUp);
 
 module.exports = router;
