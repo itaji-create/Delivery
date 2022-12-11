a61b2bef7391344a2d8c3820573adf00d3179cd3
itaji-create <itaji.1999@gmail.com>
2022-12-11 12:28:40 -0300

    feat: configura controller do usuario e cria rota user/signUp que possibilita criar novo usuario

--- back-end/src/routes/user.routes.js
+++ back-end/src/routes/user.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { signIn } = require('../controllers/user.controller');
+const { signIn, signUp } = require('../controllers/user.controller');
 
 router
-  .post('/signIn', signIn);
+  .post('/signIn', signIn)
+  .post('/signUp', signUp);
 
 module.exports = router;
