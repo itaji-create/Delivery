8ee02d71c0b18ea38b32b02ce7d9db03a94b4e43
itaji-create <itaji.1999@gmail.com>
2022-12-11 20:13:24 -0300

    feat: cria tela register com integração ao back-end, permitindo criar novo usuário

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Navigate to="/login" /> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/register" element={ <Register /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
