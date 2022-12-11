098444e717511d839f9e49547b2fbe509af58ea4
itaji-create <itaji.1999@gmail.com>
2022-12-11 18:15:21 -0300

    feat: cria tela de login e integra ela com o back-end da aplicação

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const routes = require('../routes/index')
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 routes(app);
