113e0662f2c059a57a92a014d4ac4b81f9a12d05
itaji-create <itaji.1999@gmail.com>
2023-01-06 10:41:18 -0300

    feat: cria rota para produtos

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,7 +1,9 @@
 const userRouter = require('./user.routes');
+const productsRouter = require('./products.routes');
 
 const routes = (app) => {
   app.use('/user', userRouter);
+  app.use('/products', productsRouter);
 };
 
 module.exports = routes;
