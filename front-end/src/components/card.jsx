527d7ce51eb86fdd2528050a0620cadb2ec260ab
itaji-create <itaji.1999@gmail.com>
2023-01-06 12:31:16 -0300

    feat: cria funções para adicionar e remover a quantidade de itens no carrinho de compras

--- front-end/src/components/card.jsx
+++ front-end/src/components/card.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
-function Card({ product: { price, urlImage, name } }) {
+function Card({ product: { id, price, urlImage, name } }) {
+  const addProduct = () => {
+    let cartItens = JSON.parse(localStorage.getItem('cartItens'));
+    if (!cartItens) {
+      cartItens = [];
+    }
+    if (cartItens.some((e) => e.id === id)) {
+      const prod = cartItens.find((e) => e.id === id);
+      prod.quantity += 1;
+    } else {
+      cartItens.push({ id, price, urlImage, name, quantity: 1 });
+    }
+    localStorage.setItem('cartItens', JSON.stringify(cartItens));
+  };
+
+  const removeProduct = () => {
+    let cartItens = JSON.parse(localStorage.getItem('cartItens'));
+    if (!cartItens) {
+      cartItens = [];
+    }
+    if (cartItens.some((e) => e.id === id)) {
+      const prod = cartItens.find((e) => e.id === id);
+      prod.quantity -= 1;
+      if (prod.quantity <= 0) {
+        cartItens.splice(cartItens.indexOf(prod), 1);
+      }
+    }
+    localStorage.setItem('cartItens', JSON.stringify(cartItens));
+  };
+
   return (
     <div>
       <img
@@ -11,10 +39,14 @@ function Card({ product: { price, urlImage, name } }) {
       />
       <p>{name}</p>
       <h1>{price.replace('.', ',')}</h1>
-      <button type="button">-</button>
-      <input type="text" value={ 0 } />
-      <button type="button">+</button>
+      <button type="button" onClick={ removeProduct }>-</button>
+      <button type="button" onClick={ addProduct }>+</button>
     </div>
   );
 }
+
+Card.propTypes = {
+  product: PropTypes.obj,
+}.isRequired;
+
 export default Card;
