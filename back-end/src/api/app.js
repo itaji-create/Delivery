8c8362d25aabe8c6e258c034b820f75217358048
itaji-create <itaji.1999@gmail.com>
2023-01-06 08:19:38 -0300

    feat: cria services de products que faz um get na model Products

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,6 +6,7 @@ const routes = require('../routes/index')
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..', '..', 'public')));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 routes(app);
