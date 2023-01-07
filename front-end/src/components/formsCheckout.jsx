9a6578de69692a1752e5446a39ffd277ddee26fb
itaji-create <itaji.1999@gmail.com>
2023-01-06 22:16:30 -0300

    feat: cria funções necessárias para registar a venda clicando no respectivo botão do formsCheckout

--- front-end/src/components/formsCheckout.jsx
+++ front-end/src/components/formsCheckout.jsx
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { createSale } from '../utils/requests';
 
-function Forms() {
+function Forms(props) {
+  const { total, cart } = props;
+  const navigate = useNavigate();
+  const [adress, setAdress] = useState('');
+  const [number, setNumber] = useState('');
+
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleSubmit = async () => {
+    const select = document.getElementById('vendedor');
+    const { value } = select.options[select.selectedIndex];
+
+    const data = {
+      userId: user.id,
+      sellerId: Number(value),
+      totalPrice: total,
+      deliveryAddress: adress,
+      deliveryNumber: number,
+    };
+    const sale = await createSale('/sales', data, user.token);
+
+    cart.forEach((e) => {
+      e.saleId = sale.id;
+    });
+
+    await createSale('/sales/products', cart, user.token);
+
+    navigate(`/customer/orders/${sale.id}`);
+  };
   return (
     <div>
       <div>
@@ -20,16 +51,19 @@ function Forms() {
           Endereço
           <input
             type="text"
+            onChange={ (e) => setAdress(e.target.value) }
           />
         </label>
         <label htmlFor="endereço">
           Número
           <input
             type="text"
+            onChange={ (e) => setNumber(e.target.value) }
           />
         </label>
         <button
           type="submit"
+          onClick={ handleSubmit }
         >
           Finalizar pedido
         </button>
@@ -37,5 +71,16 @@ function Forms() {
     </div>
   );
 }
+
+Forms.propTypes = {
+  total: PropTypes.number.isRequired,
+  cart: PropTypes.arrayOf({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    price: PropTypes.string,
+    qtd: PropTypes.number,
+    totalPrice: PropTypes.number,
+  }).isRequired,
+};
 
 export default Forms;
