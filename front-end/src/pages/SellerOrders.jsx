c49acc2ae96dbd27f78c1c6fb2268397f997d95b
itaji-create <itaji.1999@gmail.com>
2023-01-09 15:36:40 -0300

    feat: cria uma função fetch que imprime no console os pedidos do vendedor logado

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from 'react';
+
 function SellerOrders() {
+  const [orders, setOrders] = useState();
+
+  const fetchOrders = async () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    try {
+      const data = await requestGet(`/sales/sellerId/${user.id}`);
+      console.log(data);
+      setOrders(data);
+    } catch (error) {
+      if (error.message.includes('401')) {
+        localStorage.removeItem('user');
+        navigate('/login');
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  console.log(orders);
+
   return (
     <div>
       Ola mundo
