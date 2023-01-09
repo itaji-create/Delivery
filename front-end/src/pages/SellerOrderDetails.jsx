109434a57a43854ef1d9967275d15b8cd2f4ca56
itaji-create <itaji.1999@gmail.com>
2023-01-09 17:02:17 -0300

    feat: cria pagina que detalha pedido para vendedor

---
+++ front-end/src/pages/SellerOrderDetails.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import { requestGet } from '../utils/requests';
+
+function SellerOrderDetails() {
+  const [order, setOrder] = useState();
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    const id = window.location.href.split('orders/')[1];
+    requestGet(`sales/${id}`).then((data) => setOrder(data));
+    requestGet(`sales/products/${id}`).then((data) => setProducts(data));
+  }, []);
+
+  console.log(order);
+  return (
+    <section>
+      <h3>Detalhe do Pedido</h3>
+      {order ? (
+        <table>
+          <thead>
+            <tr>
+              <th>
+                {`Pedido  00${order.id}`}
+              </th>
+              <th>P. Vend:</th>
+              <th>
+                { order.seller.name }
+              </th>
+              <th>
+                { moment(order.saleDate).format('DD/MM/YYYY')}
+              </th>
+              <th>
+                { order.status }
+              </th>
+            </tr>
+          </thead>
+          <thead>
+            <tr>
+              <td>
+                Item
+              </td>
+              <td>Descrição</td>
+              <td>
+                Quantidade
+              </td>
+              <td>
+                Valor Unitário
+              </td>
+              <td>
+                Sub-total
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {products && products.map((e, i) => (
+              <tr key={ i }>
+                <th>
+                  { i + 1 }
+                </th>
+                <td>
+                  { order.products[i].name }
+                </td>
+                <th>
+                  { e.quantity }
+                </th>
+                <th>{ order.products[i].price }</th>
+                <th>
+                  { order.products[i].price * e.quantity }
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
+      {order && (
+        <p>
+          Total R$:
+          <i>{ order.totalPrice.replace(/\./, ',') }</i>
+        </p>)}
+      <button type="button">PREPARAR PEDIDO</button>
+      <button type="button">SAIU PARA ENTREGA </button>
+    </section>
+  );
+}
+
+export default SellerOrderDetails;
