c0796b3b9f3ed2795453e904e4f00051049bab66
itaji-create <itaji.1999@gmail.com>
2023-07-10 18:50:57 -0300

    feat: estiliza paginas de pedidos do cliente

--- front-end/src/components/orderCard.jsx
+++ front-end/src/components/orderCard.jsx
@@ -1,32 +1,24 @@
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import 'moment/locale/pt-br';
-import { useNavigate } from 'react-router-dom';
 
 function Order({ order: { id, status, saleDate, totalPrice }, role }) {
-  const navigate = useNavigate();
-
-  const handleOrders = () => {
-    navigate(`/${role}/orders/${id}`);
-  };
-
   return (
-    <button type="button" onClick={ handleOrders }>
-      <p>
-        { id }
-      </p>
-      <p>
-        { status }
-      </p>
-      <p>
-        { moment(saleDate).locale('pt-br').format('L') }
-        {/* { saleDate } */}
-      </p>
-      <p>
-        <i>R$</i>
-        { Math.abs(totalPrice).toFixed(2).toString().replace('.', ',') }
-      </p>
-    </button>
+    <div className="col-md-4">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{`Pedido #0${id}`}</h5>
+          <p className="card-text">
+            {`Data: ${moment(saleDate).locale('pt-br').format('L')}`}
+          </p>
+          <p className="bg-primary text-white p-3">{ status }</p>
+          <p className="card-text">
+            {`Preço: R$ ${Math.abs(totalPrice).toFixed(2).toString().replace('.', ',')}`}
+          </p>
+          <a href={ `/${role}/orders/${id}` } className="btn btn-primary">Detalhes</a>
+        </div>
+      </div>
+    </div>
   );
 }
 
