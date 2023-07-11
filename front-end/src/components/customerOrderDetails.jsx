058027d6d797e759341a26925e42379a93be8d12
itaji-create <itaji.1999@gmail.com>
2023-07-11 10:51:00 -0300

    feat: estiliza pagina de detalhes do pedido do cliente

--- front-end/src/components/customerOrderDetails.jsx
+++ front-end/src/components/customerOrderDetails.jsx
@@ -5,68 +5,63 @@ function Card(params) {
   const { order, products } = params;
   return (
     <div>
-      <h3>Detalhe do Pedido</h3>
+      <h3>Order Details</h3>
       {order ? (
-        <table>
-          <thead>
-            <tr>
-              <th>
-                {`Pedido  00${order.id}`}
-              </th>
-              <th>P. Vend:</th>
-              <th>
-                { order.seller.name }
-              </th>
-              <th>
-                { moment(order.saleDate).format('DD/MM/YYYY')}
-                {/* { order.saleDate } */}
-              </th>
-              <th>
-                { order.status }
-              </th>
-            </tr>
-          </thead>
-          <thead>
-            <tr>
-              <td>
-                Item
-              </td>
-              <td>Descrição</td>
-              <td>
-                Quantidade
-              </td>
-              <td>
-                Valor Unitário
-              </td>
-              <td>
-                Sub-total
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            {products && products.map((e, i) => (
-              <tr key={ i }>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <p className="navbar-brand">{`Order  #00${order.id}`}</p>
+            <p className="navbar-brand">Seller:</p>
+            <p className="navbar-brand">{ order.seller.name }</p>
+            <p className="navbar-brand">{ moment(order.saleDate).format('MM/DD/YYYY')}</p>
+            <p className="navbar-brand">{ order.status }</p>
+            <button type="button" className="btn btn-success rounded-pill px-3">
+              Mark as delivered
+            </button>
+          </nav>
+          <table className="table">
+            <thead>
+              <tr>
                 <th>
-                  { i + 1 }
+                  Item
                 </th>
-                <td>
-                  { order.products[i].name }
-                </td>
+                <th>Descrição</th>
                 <th>
-                  { e.quantity }
+                  Quantidade
                 </th>
-                <th>{ order.products[i].price }</th>
                 <th>
-                  { order.products[i].price * e.quantity }
+                  Valor Unitário
+                </th>
+                <th>
+                  Sub-total
                 </th>
               </tr>
-            ))}
-          </tbody>
-          <p>
-            Totallllll R$:
-            <i>{ order.totalPrice.replace(/\./, ',') }</i>
-          </p>
-        </table>
+            </thead>
+            <tbody>
+              {products && products.map((e, i) => (
+                <tr key={ i }>
+                  <th>
+                    { i + 1 }
+                  </th>
+                  <td>
+                    { order.products[i].name }
+                  </td>
+                  <th>
+                    { e.quantity }
+                  </th>
+                  <th>{ order.products[i].price }</th>
+                  <th>
+                    { (order.products[i].price * e.quantity).toFixed(2) }
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <h3>
+            {
+              `Total: R$ ${(order.totalPrice).replace('.', ',')}`
+            }
+          </h3>
+        </div>
       ) : null}
     </div>
   );
